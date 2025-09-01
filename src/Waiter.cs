d5f3388797309561b1560730567366269dbafa9c
brunekpereda <BRUNO.PEREDA@correo.ucu.edu.uy>
2025-09-01 14:14:15 -0300

    Implemento Waiter

--- src/Waiter.cs
+++ src/Waiter.cs
@@ -8,5 +8,22 @@ namespace Ucu.Poo.Restaurant
     public class Waiter
     {
         private List<Table> assignedTables = new List<Table>();
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+
+        public void AssignTable(Table table)
+        {
+            assignedTables.Add(table);
+        }
+
+        public void TakeOrder(Table table, Dish dish)
+        {
+            table.AddToOrder(dish);
+        }
     }
 }
